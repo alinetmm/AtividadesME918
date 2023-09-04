91e63ea7914048b30b47f03159fa55c78a4655ae
alinetmm <142602001+alinetmm@users.noreply.github.com>
2023-09-04 14:59:13 -0300

    Update executa.R

--- executa.R
+++ executa.R
@@ -4,6 +4,6 @@ source("simulador.R")
 devtools::source_url("https://ime.unicamp.br/~ra137784/ME918/2023s2/lab01_ig.R")
 planos <- input_lab01()
 
-purrr::map(planos, simulador)
+amostras <- purrr::map(planos, f)
 
 
