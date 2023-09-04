b7d1d47a3f83773964e6825e70e9f07935e46951
alinetmm <142602001+alinetmm@users.noreply.github.com>
2023-09-04 14:50:56 -0300

    Update registra.R

--- registra.R
+++ registra.R
@@ -1,14 +1,21 @@
 source("executa.r")
 
-id = 01:33
-
-registra <- function(id){
-  obs = planos[[id]]
-  fun = data.frame(result= simulador(obs))
-  resultado = c(obs, fun)
-  return(save(resultado, file = paste0("simulação", id, ".RData")))
-}
-
-setwd("resultados")
-purrr::walk(id,registra)
-
+purrr::walk(seq_along(planos), function(i){
+  fname = paste0("resultados/simulação", i, ".RData")
+  distribution <- planos[[i]]$distribution
+  result <- amostras[[i]]
+  
+  if (distribution == "poisson"){
+    lambda <- planos[[i]]$lambda
+    save(distribution, lambda, result, file = fname)
+    
+  } else if(distribution == "bernoulli") {
+    p <- planos[[i]]$p
+    save(distribution, p, result, file = fname)
+    
+  } else {
+    mu <- planos[[i]]$mu
+    sigma2 <- planos[[i]]$sigma2
+    save(distribution, mu, sigma2, result, file = fname)
+  }
+})
